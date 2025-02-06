905f2500b5ec04e0bfa41cd274f964d74187beda
satyajeet <satyjeetzende6408@gmail.com>
2025-02-06 15:36:13 +0530

    my first commit

---
+++ factorial1.py
@@ -0,0 +1,11 @@
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+# Example usage
+num = int(input("Enter a number: "))
+result = factorial(num)
+print(f"The factorial of {num} is {result}")
+
